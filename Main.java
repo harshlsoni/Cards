89b8953413dab3093f9a73c18e30f3ae0789ddeb
harshlsoni <harshlsoni2920@gmail.com>
2025-02-04 14:23:27 +0530

    main.java created

--- Main.java
+++ Main.java
@@ -4,5 +4,9 @@ import java.util.*;
 class Main{
     public static void main(String args[]) {
         Deck deck = new Deck();
+        System.out.println("*****Creating Deck******");
+        deck.createDeck();
+        System.out.println("*****Print Deck******");
+        deck.displayDeck();
     }
 }
