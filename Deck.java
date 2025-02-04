14d14e11988b59a1b6528acd60f222b63ce6af5d
harshlsoni <harshlsoni2920@gmail.com>
2025-02-04 14:11:20 +0530

    CreateDeck() created

--- Deck.java
+++ Deck.java
@@ -8,6 +8,11 @@ class Deck{
 		this.deck = new ArrayList<>();
 	}
 
+	public void CreateDeck() {
+		String[] ranks = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+		String[] suits = {"Clubs","Diamonds","Hearts","Spades"};
+	}
+
 
 
 }
